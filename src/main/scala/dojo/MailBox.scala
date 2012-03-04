faf844ee975446987618e9089b73c03fb2f3b59e
Caoilte O'Connor <caoilte@gmail.com>
2012-03-04 22:28:56 +0000

    changes from Friday

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,9 +3,15 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
+    case Ping => Pong
+    case Pong => Ping
     case _ => None
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) = msg match {
+    case msg:String => "stopped"
+    case ("echo",response) => response
+    case msg:PingPongMessage => handleMsg(msg)
+  }
 
 }
