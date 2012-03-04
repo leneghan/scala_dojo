d4fb8cc4756a8188b3f8129bff160102f81e8693
Caoilte O'Connor <caoilte@gmail.com>
2012-03-04 22:50:31 +0000

    Revert "changes from Friday"
    This reverts commit faf844ee975446987618e9089b73c03fb2f3b59e.

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,15 +3,9 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
-    case Ping => Pong
-    case Pong => Ping
     case _ => None
   }
 
-  def receive(msg: Any) = msg match {
-    case msg:String => "stopped"
-    case ("echo",response) => response
-    case msg:PingPongMessage => handleMsg(msg)
-  }
+  def receive(msg: Any) = None
 
 }
